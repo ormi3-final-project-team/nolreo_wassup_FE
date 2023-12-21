e34204883697f22d0bfabca1573a975d89a6c1ae
ggengmo <rudah365dlf@gmail.com>
2023-12-21 23:07:45 +0900

    :sparkles: Feat: 메인 페이지 navbar 수정
    - 수정 사항
    메인 페이지 navbar 수정

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -121,39 +121,39 @@ $(document).ready(function(){
         // i. #testimonial-carousel
     
         
-        var owl=$('#testemonial-carousel');
-        owl.owlCarousel({
-            items:3,
-            margin:0,
+        // var owl=$('#testemonial-carousel');
+        // owl.owlCarousel({
+        //     items:3,
+        //     margin:0,
             
-            loop:true,
-            autoplay:true,
-            smartSpeed:1000,
+        //     loop:true,
+        //     autoplay:true,
+        //     smartSpeed:1000,
             
-            //nav:false,
-            //navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        //     //nav:false,
+        //     //navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
             
-            dots:true,
-            autoplayHoverPause:true,
+        //     dots:true,
+        //     autoplayHoverPause:true,
         
-            responsiveClass:true,
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    640:{
-                        items:1
-                    },
-                    767:{
-                        items:2
-                    },
-                    992:{
-                        items:3
-                    }
-                }
+        //     responsiveClass:true,
+        //         responsive:{
+        //             0:{
+        //                 items:1
+        //             },
+        //             640:{
+        //                 items:1
+        //             },
+        //             767:{
+        //                 items:2
+        //             },
+        //             992:{
+        //                 items:3
+        //             }
+        //         }
             
             
-        });
+        // });
 
     // 5. datepicker
             $('[data-toggle="datepicker"]').datepicker();
